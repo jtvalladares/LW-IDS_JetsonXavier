71e703c623f7c336b41aa3ef4c14958d6eee47c7
jtvalladares <jtvalladares@uc.cl>
2025-05-23 15:40:54 -0400

    subir archivos

---
+++ Host/src/params.py
@@ -0,0 +1,10 @@
+import os
+
+IP_HOST = os.getenv("IP_HOST", "10.8.33.137")
+PORT = int(os.getenv("PORT", 7631))
+
+NUM_EXP = int(os.getenv("NUM_EXP", 1))
+NUM_TEST = int(os.getenv("NUM_TEST", 2))
+NUM_CLASS = int(os.getenv("NUM_CLASS", 1))
+
+NUM_REGS = int(os.getenv("NUM_REGS", 117177))
